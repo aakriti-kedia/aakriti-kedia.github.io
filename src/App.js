7fc9b2e6c61b58dbd9cf8a38fd1f8ef329220e7b
Aakriti Kedia <akedia614@gmail.com>
2022-12-05 15:48:48 -0800

    router boilerplate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import React, { Component }  from 'react';
-import './App.css';
+import React  from 'react';
 
-function App() {
+import './App.css';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/home'
+import Recommendations from './pages/recommendations'
+import About from './pages/about'
+import Resume from './pages/resume'
+import Contact from './pages/contact'
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/recommendations" element={<Recommendations/>} />
+        <Route exact path="/about" element={<About/>} />
+        <Route exact path="/resume" element={<Resume/>} />
+        <Route exact path="/contact" element={<Contact/>} />
+        {/* <Route exact path="*" element={<p>Path not resolved</p>} /> */}
+        <Route exact path="/somepath" element={ Recommendations } />
+      </Routes>          
+    </HashRouter>
   );
 }
 
---
+++ src/pages/recommendations.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+const Recommendations = () => {
+    console.log("Recommendations called");
+    return (
+        <div> Hello from Recommendations!!</div>
+    );
+}
+export default Recommendations;
