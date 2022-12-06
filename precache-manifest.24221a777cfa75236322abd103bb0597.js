4c2b7378343def500ab34628ac97198b62286f68
Aakriti Kedia <akedia614@gmail.com>
2022-12-05 18:49:47 -0800

    Updates

---
+++ precache-manifest.24221a777cfa75236322abd103bb0597.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "763465e52d63d8fa80cb",
+    "url": "/static/css/main.037f6cb5.chunk.css"
+  },
+  {
+    "revision": "763465e52d63d8fa80cb",
+    "url": "/static/js/main.763465e5.chunk.js"
+  },
+  {
+    "revision": "e45e0075b0567c8b67a9",
+    "url": "/static/js/1.e45e0075.chunk.js"
+  },
+  {
+    "revision": "ae58a5faef66e8707967",
+    "url": "/static/js/2.ae58a5fa.chunk.js"
+  },
+  {
+    "revision": "9f01a62a234a6fc127f8",
+    "url": "/static/js/runtime~main.9f01a62a.js"
+  },
+  {
+    "revision": "5a349dc200afa71c7a8f6112b79b05b7",
+    "url": "/index.html"
+  }
+];
