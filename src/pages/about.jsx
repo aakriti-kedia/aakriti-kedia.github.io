c53ad72df7101ccc3f380a2c88bb7675b5043b6b
Aakriti Kedia <akedia614@gmail.com>
2022-12-06 11:12:20 -0800

    about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from "react";
+
+import "../styles/about.css";
+
 const About = () => {
-    return (
-        <div> Hello from About!!</div>
-    );
+  return (
+    <div className="about-container">
+      <div className="about-intro">
+        <p className="about-details">
+          I am a first year graduate student at University of California, San Diego, majoring in Computer Science with a specialization in Artificial Intelligence.
+          I have completed my undergrad from Birla Institute of Technology, Mesra, and have worked at Walmart Global Tech, Bangalore for 2 years.
+        </p>
+        <p className="about-details">
+        I am an enthusiastic software engineer with 2+ years of experience in the E-commerce domain. 
+          I have worked across diverse strategic and engineering initiatives, focused on software development, 
+          APIs creation, real time data updates, outlier detection, pipelines etc, along with building autoscaling and alerting platforms, 
+          and have hands-on with Splunk, Grafana, Jmeter, Walmart Cloud, etc.
+          I am passionate about enhancing my technical expertise in the software engineering domain and 
+          contributing to seamless customer experience.
+        </p>
+        <p className="about-details">
+        </p>
+
+        <hr />
+
+        <p className="about-quote">
+        </p>
+      </div>
+    </div>
+  );
 }
+
 export default About;
