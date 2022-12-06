81deec287de830e7717c68ced1cff7142df9f3ec
Aakriti Kedia <akedia614@gmail.com>
2022-12-06 11:45:01 -0800

    resume

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Pdf from '../assets/pdfs/resume.pdf';
 const Resume = () => {
     return (
-        <div> Hello from Resume!!</div>
+        <div className="about-container">
+        <div className="about-intro">
+        <div> 
+            <a href = {Pdf} target = "_blank">Download Pdf</a>
+        </div>
+        </div>
+        </div>
     );
 }
 export default Resume;
